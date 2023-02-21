3b6e8fc30db4c68e94cc82a84b96705723f574c1
Jacob Zimmerman <jacob.zimmerman135@gmail.com>
2023-02-21 08:33:04 -0500

    end of tuesday feb 21, printf calls work but still need to fix timing

--- src/jit/implib.rs
+++ src/jit/implib.rs
@@ -1,4 +1,5 @@
 use super::*;
+use crate::ast::AstType;
 
 pub fn build_impcore_printers<'ctx>(compiler: &mut Compiler<'ctx>) {
     let context = &compiler.context;
@@ -6,53 +7,41 @@ pub fn build_impcore_printers<'ctx>(compiler: &mut Compiler<'ctx>) {
     let builder = &compiler.builder;
 
     let int_type = context.i32_type();
-    let printf_fn = *compiler.lib.get("printf").unwrap();
+    let ptr_type = int_type.ptr_type(AddressSpace::default());
+    let printf_fn = *compiler
+        .lib
+        .get("printf")
+        .expect("Trying to build implib printers without printf");
+
     let impcore_printers = [
-        ("println", "%i\n", "fmt_ln"),
-        ("print", "%i", "fmt_i"),
-        ("printu", "%u", "fmt_u"),
+        ("println", "%i\n", "fmt_ln", AstType::Integer),
+        ("print", "%i", "fmt_i", AstType::Integer),
+        ("printu", "%u", "fmt_u", AstType::Integer),
+        ("printstr", "%s", "fmt_str", AstType::Pointer),
     ];
 
-    for (printer_name, fmt_str, fmt) in impcore_printers {
-        let unary_type = int_type.fn_type(&[int_type.into()], false);
+    for (printer_name, fmt_str, fmt, arg_type) in impcore_printers {
+        let unary_type = int_type.fn_type(
+            &[match arg_type {
+                AstType::Pointer => ptr_type.into(),
+                AstType::Integer => int_type.into(),
+            }],
+            false,
+        );
         let print_fn = module.add_function(printer_name, unary_type, None);
         let block = context.append_basic_block(print_fn, "entry");
         builder.position_at_end(block);
-        let int_arg = print_fn.get_first_param().unwrap().into_int_value();
         let fmt_ptr = builder
             .build_global_string_ptr(fmt_str, fmt)
             .as_pointer_value();
-        builder.build_call(printf_fn, &[fmt_ptr.into(), int_arg.into()], "printfcall");
-        builder.build_return(Some(&int_arg));
+        let param = print_fn.get_first_param().unwrap();
+        builder.build_call(printf_fn, &[fmt_ptr.into(), param.into()], "printfcall");
+        let retval = match arg_type {
+            AstType::Integer => param.into_int_value(),
+            AstType::Pointer => int_type.const_zero(),
+        };
+        builder.build_return(Some(&retval));
         compiler.fpm.run_on(&print_fn);
         compiler.lib.insert(printer_name, print_fn);
     }
-
-    build_printstr(compiler);
-}
-
-pub fn build_printstr<'ctx>(compiler: &mut Compiler<'ctx>) {
-    let context = &compiler.context;
-    let module = &compiler.module;
-    let builder = &compiler.builder;
-
-    let int_type = context.i32_type();
-    let ptr_type = int_type.ptr_type(AddressSpace::default());
-    let str_type = context.i8_type().ptr_type(AddressSpace::default());
-    let printf_fn = *compiler.lib.get("printf").unwrap();
-
-    let unary_type = int_type.fn_type(&[ptr_type.into()], false);
-    let print_fn = module.add_function("printstr", unary_type, None);
-    let block = context.append_basic_block(print_fn, "entry");
-    builder.position_at_end(block);
-
-    let iptr_arg = print_fn.get_first_param().unwrap().into_pointer_value();
-    let fmt_ptr = builder
-        .build_global_string_ptr("%s", "fmt_str")
-        .as_pointer_value();
-
-    let arg_ptr = builder.build_bitcast(iptr_arg, str_type, "cast");
-    builder.build_call(printf_fn, &[fmt_ptr.into(), arg_ptr.into()], "printfcall");
-    builder.build_return(Some(&int_type.const_zero()));
-    compiler.lib.insert("printstr", print_fn);
 }
