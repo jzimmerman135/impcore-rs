30a1c9aabaced2c2b72b952c5258672aa3f02921
Jacob Zimmerman <jacob.zimmerman135@gmail.com>
2023-04-04 14:51:17 -0400

    removed redundant passes

--- src/jit.rs
+++ src/jit.rs
@@ -136,8 +136,6 @@ impl<'ctx> Compiler<'ctx> {
         fpm.add_cfg_simplification_pass();
         fpm.add_basic_alias_analysis_pass();
         fpm.add_promote_memory_to_register_pass();
-        fpm.add_instruction_combining_pass();
-        fpm.add_reassociate_pass();
         fpm.add_tail_call_elimination_pass();
         fpm.initialize();
         fpm
